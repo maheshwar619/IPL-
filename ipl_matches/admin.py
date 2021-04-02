6d40c2588dcfdb24059476cf02530fbe7f3810bd
maheshwar619 <81235779+maheshwar619@users.noreply.github.com>
2021-04-02 13:09:29 +0530

    Add files via upload

---
+++ ipl_matches/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Task
+
+
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     pass
+
+
+
+admin.site.register(Task)
+# Register your models here.
+
---
+++ ipl_matches/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IplMatchesConfig(AppConfig):
+    name = 'ipl_matches'
